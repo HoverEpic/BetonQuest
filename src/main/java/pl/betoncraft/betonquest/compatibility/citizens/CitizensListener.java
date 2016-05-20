1c1c2b2773a73ea6e045e7a562d6d2ece3497354
HoverEpic <vincent.c@millenium-studio.fr>
2016-05-20 14:53:58 +0200

    Add right/left click on citizens NPC

--- src/main/java/pl/betoncraft/betonquest/compatibility/citizens/CitizensListener.java
+++ src/main/java/pl/betoncraft/betonquest/compatibility/citizens/CitizensListener.java
@@ -1,22 +1,23 @@
 /**
- * BetonQuest - advanced quests for Bukkit
- * Copyright (C) 2015  Jakub "Co0sh" Sapalski
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * BetonQuest - advanced quests for Bukkit Copyright (C) 2015 Jakub "Co0sh"
+ * Sapalski
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package pl.betoncraft.betonquest.compatibility.citizens;
 
+import net.citizensnpcs.api.event.NPCLeftClickEvent;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
 
 import org.bukkit.Bukkit;
@@ -31,37 +32,74 @@ import pl.betoncraft.betonquest.utils.PlayerConverter;
 
 /**
  * Starts new conversations with NPCs
- * 
+ *
  * @author Jakub Sapalski
  */
-public class CitizensListener implements Listener {
+public class CitizensListener implements Listener
+{
 
-	/**
-	 * Initializes the listener
-	 */
-	public CitizensListener() {
-		Bukkit.getPluginManager().registerEvents(this, BetonQuest.getInstance());
-	}
+    private BetonQuest plugin;
 
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onNPCClick(final NPCRightClickEvent event) {
-		if (event.isCancelled()) {
-			return;
-		}
-		final String playerID = PlayerConverter.getID(event.getClicker());
-		if (CombatTagger.isTagged(playerID)) {
-			Config.sendMessage(playerID, "busy");
-			return;
-		}
-		String id = String.valueOf(event.getNPC().getId());
-		String assignment = Config.getNpc(id);
-		if (assignment != null) {
-			String[] parts = assignment.split("\\.");
-			final String convName = parts[1];
-			final String packName = parts[0];
-			event.setCancelled(true);
-			new CitizensConversation(playerID, packName, convName, event.getNPC().getEntity().getLocation(),
-					event.getNPC());
-		}
-	}
+    /**
+     * Initializes the listener
+     */
+    public CitizensListener()
+    {
+        this.plugin = BetonQuest.getInstance();
+        Bukkit.getPluginManager().registerEvents(this, BetonQuest.getInstance());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onNPCRightClick(final NPCRightClickEvent event)
+    {
+        if (this.plugin.getConfig().getString("click.right").equalsIgnoreCase("false"))
+        {
+            return;
+        }
+        final String playerID = PlayerConverter.getID(event.getClicker());
+        if (CombatTagger.isTagged(playerID))
+        {
+            Config.sendMessage(playerID, "busy");
+            return;
+        }
+        String id = String.valueOf(event.getNPC().getId());
+        String assignment = Config.getNpc(id);
+        if (assignment != null)
+        {
+            String[] parts = assignment.split("\\.");
+            final String convName = parts[1];
+            final String packName = parts[0];
+            event.setCancelled(true);
+            new CitizensConversation(playerID, packName, convName, event.getNPC().getEntity().getLocation(), event.getNPC());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onNPCLeftClick(final NPCLeftClickEvent event)
+    {
+        if (this.plugin.getConfig().getString("click.left").equalsIgnoreCase("false"))
+        {
+            return;
+        }
+        if (event.isCancelled())
+        {
+            return;
+        }
+        final String playerID = PlayerConverter.getID(event.getClicker());
+        if (CombatTagger.isTagged(playerID))
+        {
+            Config.sendMessage(playerID, "busy");
+            return;
+        }
+        final String id = String.valueOf(event.getNPC().getId());
+        final String assignment = Config.getNpc(id);
+        if (assignment != null)
+        {
+            final String[] parts = assignment.split("\\.");
+            final String convName = parts[1];
+            final String packName = parts[0];
+            event.setCancelled(true);
+            new CitizensConversation(playerID, packName, convName, event.getNPC().getEntity().getLocation(), event.getNPC());
+        }
+    }
 }
